c5fe96ebea6b36588b535a7c69c09ae2330075a5
dotoanhaui <48145580+dotoanhaui@users.noreply.github.com>
2020-11-07 10:03:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <h1> Bài 01: Sử dụng các tag cơ bản</h1>
+    <h1 style="text-decoration: underline">Yêu cầu:</h1>
+    <p>
+        <i>
+            Thiết kế trang web có nội dung như sau:
+        </i>
+    </p>
+    <p align="center">Lập trình web<br> Chào mừng bạn đến với ngôn ngữ HTML</p>
+</div>
+</body>
+</html>
